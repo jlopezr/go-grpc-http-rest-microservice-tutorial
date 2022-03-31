c169a72501eb149560a7b729c0e2fbe1c0e63ded
Juan López Rubio <j.lopez.r@gmail.com>
2022-03-31 21:17:44 +0200

    added client and mariadb scripts

---
+++ create-tables.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u root < create_tables.sql
---
+++ install-mariadb.sh
@@ -0,0 +1,4 @@
+sudo port install mariadb mariadb-server
+sudo port load mariadb-server
+sudo port load mariadb-server
+sudo -u _mysql /opt/local/lib/mariadb/bin/mysql_install_db
