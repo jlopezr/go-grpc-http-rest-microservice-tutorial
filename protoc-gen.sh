058017e6dfc1d869c06cc2b33d1aebb019e97581
Juan López Rubio <j.lopez.r@gmail.com>
2022-04-01 10:13:42 +0200

    added swagger & grpcgw generator

--- protoc-gen.sh
+++ protoc-gen.sh
@@ -1,3 +1,5 @@
 protoc todo-service.proto --proto_path=api/proto/v1 --proto_path=third-party \
     --go_out=./pkg/api/v1 --go_opt=paths=source_relative \
-    --go-grpc_out=./pkg/api/v1 --go-grpc_opt=paths=source_relative
+    --go-grpc_out=./pkg/api/v1 --go-grpc_opt=paths=source_relative \
+    --grpc-gateway_out ./pkg/api/v1 --grpc-gateway_opt paths=source_relative --grpc-gateway_opt logtostderr=true \
+    --openapiv2_out ./pkg/api/v1 --openapiv2_opt logtostderr=true \
